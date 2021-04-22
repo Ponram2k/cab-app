18e5f0cd9f26ead7bfe3ad322bd34184dfb8d2a7
Ponram2k <82449745+Ponram2k@users.noreply.github.com>
2021-04-22 09:54:18 +0530

    Update PeakTime.java

--- src/com/ola/PeakTime.java
+++ src/com/ola/PeakTime.java
@@ -9,7 +9,7 @@ public class PeakTime {
 	 * @return true whether the given time is peak time
 	 */
 	public static boolean getPeakTime(String time) {
-		LocalTime pekTym = GetDateAndTime.convertStringToTime(time);
+		LocalTime pekTym = UtilDateTime.convertStringToTime(time);
 		int check = pekTym.getHour();
 		if (check >= 17 && check <= 19) {
 			return true;
